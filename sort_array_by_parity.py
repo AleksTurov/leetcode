584ed40557f50170c9c6490b085bb3f4cd39f7a9
ALEKSEI <119919002+AleksTurov@users.noreply.github.com>
2024-07-16 07:37:56 +0000

    2024-07-16

---
+++ sort_array_by_parity.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        even_index = 0 
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[i], nums[even_index] = nums[even_index], nums[i]
+                even_index += 1
+
+        return nums
+
+# Тестирование
+nums = [3, 1, 2, 4]
+solution = Solution()
+result = solution.sortArrayByParity(nums)
+
+print(result)  # Output: [2, 4, 3, 1]
