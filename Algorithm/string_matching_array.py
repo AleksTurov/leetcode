f89e7a352874ebe346a838e1ea2876a4aab78cbb
ALEKSEI <119919002+AleksTurov@users.noreply.github.com>
2025-01-10 03:15:11 +0000

    feat: Add new algorithm for string matching in an array and update README

---
+++ Algorithm/string_matching_array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        result = []
+        for i, word1 in enumerate(words):
+            for j, word2 in enumerate(words):
+                if i != j and word1 in word2:
+                    result.append(word1)
+                    break
+        return result
+    
+solution = Solution()
+words1 = ["mass","as","hero","superhero"]
+print(solution.stringMatching(words1))
+
+words2 = ["leetcode","et","code"]
+print(solution.stringMatching(words2))
+
+words3 = ["blue","green","bu"]
+print(solution.stringMatching(words3))
