a2bc63e3fa77c5c8f85975cb6856b6247acbd1cd
ALEKSEI <119919002+AleksTurov@users.noreply.github.com>
2025-01-24 02:38:30 +0000

    feat: Optimize number of good pairs algorithm and update README with time complexity

--- Algorithm/number_good_pairs.py
+++ Algorithm/number_good_pairs.py
@@ -4,13 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        
+        count = defaultdict(int)
         good_pairs = 0
-        for i, num in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if i < j and num == num2:
-                    good_pairs += 1
+        
+        for num in nums:
+            good_pairs += count[num]
+            count[num] += 1
+        
         return good_pairs
 
-solution = Solution()
-nums1 = [1,2,3,1,1,3]
-print(solution.numIdenticalPairs(nums1))
+solution = Solution()   
+print(solution.numIdenticalPairs([1,2,3,1,1,3])) # 4
+print(solution.numIdenticalPairs([1,1,1,1])) # 6
